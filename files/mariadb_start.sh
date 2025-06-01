41740e0a6f9a86222f961ff00b0ca8f9e08ef743
Brad House <brad@brad-house.com>
2025-06-01 08:06:36 -0400

    detect changed

--- files/mariadb_start.sh
+++ files/mariadb_start.sh
@@ -6,20 +6,25 @@ STOP=$1
 CLUSTERWAIT=$2
 MAX_DURATION=1800
 DELAY=10
+CHANGED=0
 
 if [ "$STOP" = "1" ] ; then
   # Make sure mariadb is stopped
+  CHANGED=1
   systemctl stop mariadb
 fi
 
 # Start it
-systemctl start mariadb
+if ! systemctl is-active mariadb ; then
+  CHANGED=1
+  systemctl start mariadb
+fi
 
 if [ "$CLUSTERWAIT" = "1" ] ; then
   start=`date +%s`
   sleep ${DELAY}
   while ! /usr/local/bin/clustercheck > /dev/null ; do
-    if [ ! systemctl is-active mariadb > /dev/null ] ; then
+    if ! systemctl is-active mariadb > /dev/null ; then
       echo "MariaDB unexpectedly quit"
       exit 1
     fi
@@ -31,6 +36,10 @@ if [ "$CLUSTERWAIT" = "1" ] ; then
     fi
     sleep ${DELAY}
   done
+fi
+
+if [ "$CHANGED" = "1" ] ; then
+  echo "changed"
 fi
 
 exit 0
